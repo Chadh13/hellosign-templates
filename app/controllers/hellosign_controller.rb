19c68283799ec85c8b54183cd7b5f900f2ee3fa2
Chad Heinen <chad@velocis.us>
2015-08-14 14:35:57 -0500

    Try callback response with text: instead of json: for string.

--- app/controllers/hellosign_controller.rb
+++ app/controllers/hellosign_controller.rb
@@ -1,9 +1,8 @@
 class HellosignController < ApplicationController
 
 	def callbacks
-		HelloSignEvent.new(params[:json])
 		respond_to do |format|
-			format.json { render json: 'Hello API Event Received', status: 200 }
+			format.json { render text: 'Hello API Event Received', status: 200 }
 		end
 	end
 
